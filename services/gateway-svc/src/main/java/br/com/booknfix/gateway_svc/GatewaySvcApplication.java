e79f7f296e13e14cd7e224e0b825f2c45969f5c8
Claudio712005 <clausilvaaraujo11@gmail.com>
2025-08-09 12:49:36 -0300

    Adicionando configuração inicial do gateway

--- services/gateway-svc/src/main/java/br/com/booknfix/gateway_svc/GatewaySvcApplication.java
+++ services/gateway-svc/src/main/java/br/com/booknfix/gateway_svc/GatewaySvcApplication.java
@@ -2,8 +2,10 @@ package br.com.booknfix.gateway_svc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewaySvcApplication {
 
 	public static void main(String[] args) {
